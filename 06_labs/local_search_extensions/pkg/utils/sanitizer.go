150f3635ad446b16e7a99565055f535bc096ee26
Magda <madzia0804@gmail.com>
2025-11-19 22:54:19 +0100

    FEATURE: implementation of MSLS and ILS extensions

---
+++ 06_labs/local_search_extensions/pkg/utils/sanitizer.go
@@ -0,0 +1,8 @@
+package utils
+
+import "strings"
+
+func SanitizeFileName(name string) string {
+	replacer := strings.NewReplacer(" ", "_", "(", "", ")", "", ",", "")
+	return replacer.Replace(name)
+}
